e46c5d4e2e047df28bae015ab7940ebb35101743
Olga Lyubicheva <forjobslov@gmail.com>
2019-07-03 11:13:50 +0300

    Updates

---
+++ precache-manifest.34d705c75833fa8048d7f4f6992b2dee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19b998bf046a94c663c38a8eb636f9c8",
+    "url": "/react_static-list-of-posts/index.html"
+  },
+  {
+    "revision": "4101d086c1038b0c1924",
+    "url": "/react_static-list-of-posts/static/css/main.628f9afb.chunk.css"
+  },
+  {
+    "revision": "0d93b29b27d1d199691f",
+    "url": "/react_static-list-of-posts/static/js/2.95678282.chunk.js"
+  },
+  {
+    "revision": "4101d086c1038b0c1924",
+    "url": "/react_static-list-of-posts/static/js/main.582a8841.chunk.js"
+  },
+  {
+    "revision": "3fdea9cdc8e2a1e9a77e",
+    "url": "/react_static-list-of-posts/static/js/runtime~main.c48bb6e1.js"
+  }
+]);
